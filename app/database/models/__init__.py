8dc8f2981d13b24b97d26d81fedb9ec9c1dde07d
salimjonovmuhammadqodir07-ux <salimjonovmuhammadqodir07@gmail.com>
2026-09-06 15:14:08 +0500

    Import models from anime, payment, and user modules

--- app/database/models/__init__.py
+++ app/database/models/__init__.py
@@ -1,1 +1,3 @@
-
+from app.database.models.anime import Anime, Episode, Favorite, WatchHistory
+from app.database.models.payment import BotSetting, PaymentRequest, RequiredChannel
+from app.database.models.user import User
