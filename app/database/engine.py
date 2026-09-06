785e39aca4678e56d3559ca4e0dd9ade680bd6f0
salimjonovmuhammadqodir07-ux <salimjonovmuhammadqodir07@gmail.com>
2026-09-06 16:55:40 +0500

    Update init_models to drop and recreate tables
    Modified init_models function to drop existing tables before creating new ones.

--- app/database/engine.py
+++ app/database/engine.py
@@ -15,7 +15,8 @@ engine = create_async_engine(settings.DATABASE_URL, echo=False, pool_pre_ping=Tr
 async_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 
-async def init_models() -> None:
+ async def init_models() -> None:
     """Ilk ishga tushirishda jadvallarni yaratadi (production'da Alembic tavsiya etiladi)."""
     async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
